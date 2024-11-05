42c6833675127c0526a6933f5c6173b27c35b0c2
asim-wecodethat <asim@wecodethat.com>
2024-11-05 17:41:15 +0530

    Implement AJAX-based follow/unfollow functionality
    - Add 'user_follow' view in views.py to handle follow/unfollow requests with AJAX.
    - Include JSON response handling for success and error states.
    - Update 'urls.py' to define the user_follow URL pattern, ensuring correct URL matching order.
    - Add follow/unfollow button and followers count update in 'user/detail.html'.
    - Use jQuery to toggle follow/unfollow button text and dynamically update follower count on the frontend.
    - Improves user experience with seamless follow/unfollow interaction without page reload.

--- account/views.py
+++ account/views.py
@@ -1,10 +1,17 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
+
+from common.decorators import ajax_required
 
 from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
-from .models import Profile
+from .models import Contact, Profile
+
+User = get_user_model()  # noqa
 
 
 @login_required
@@ -67,3 +74,22 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
