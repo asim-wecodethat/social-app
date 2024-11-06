fadf54deb2edefe76c5395f7c7079f56a1ddb11c
asim-wecodethat <asim@wecodethat.com>
2024-11-06 11:04:02 +0530

    Integrate Redis for item view tracking and ranking system
    - Installed and configured Redis for the project.
    - Implemented functionality to store and increment item views using Redis.
    - Created a ranking system using Redis sorted sets to track and display most viewed items.
    - Refactored views to use Redis for performance optimization, reducing database load.
    - Updated templates to show real-time views and rankings of items.

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
+import redis
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -10,6 +12,10 @@ from common.decorators import ajax_required
 
 from .forms import ImageCreateForm
 from .models import Image
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -32,8 +38,19 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+
+    total_views = r.incr(
+        f"image:{image.id}:views"
+    )  # Increment the total image views by 1
+
+    r.zincrby(
+        "image_ranking", 1, image.id
+    )  # Increment the image ranking by 1 in the sorted set 'image_ranking'
+
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
@@ -62,14 +79,38 @@ def image_list(request):
     images = Image.objects.all()
     paginator = Paginator(images, 8)
     page = request.GET.get("page")
+
     try:
         images = paginator.page(page)
     except PageNotAnInteger:
         images = paginator.page(1)
     except EmptyPage:
-        if request.is_ajax():
+        if request.headers.get("X-Requested-With") == "XMLHttpRequest":
             return HttpResponse("")
         images = paginator.page(paginator.num_pages)
-    if request.is_ajax():
+
+    if request.headers.get("X-Requested-With") == "XMLHttpRequest":
         return render(request, "images/image/list_ajax.html", {"images": images})
+
     return render(request, "images/image/list.html", {"images": images})
+
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary sorted by views (descending)
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+
+    # Get the IDs of the most viewed images
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    # Get the most viewed Image objects for the retrieved IDs
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+
+    # Sort the images by their ranking order
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed},
+    )
